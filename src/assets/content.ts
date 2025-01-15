36c5d7c9fd76e04122a18e4b8775df449fe81dd2
Jose Alejandro <jose-a-mendez@hotmail.com>
2025-01-15 11:44:37 -0300

    Create file with example content

---
+++ src/assets/content.ts
@@ -0,0 +1,6 @@
+export const Text = [
+  "La sinopsis de un libro es un resumen breve de los elementos principales de la obra. En ella se destacan los momentos más importantes de la trama, los personajes principales y su desarrollo.",
+  "La sinopsis de un libro famoso puede ayudar a los lectores a comprender la historia y decidir si quieren leerlo.",
+  "Un ejemplo de sinopsis de un libro famoso es la de La Odisea de Homero. En esta obra, se narra el viaje de Ulises para volver a su patria, Ítaca, después de la conquista de Troya.",
+  "La Odisea es un poema épico que recoge leyendas y cuentos populares. En él se describe el viaje de Ulises, que se extiende por diez años, y las aventuras que vive junto a su tripulación.",
+];

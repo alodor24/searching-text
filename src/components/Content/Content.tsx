13fa50ee7f07a1ef91d1a8a7fbe13192e817dded
Jose Alejandro <jose-a-mendez@hotmail.com>
2025-01-16 01:01:07 -0300

    Adjust Content component to save count highlightText

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,15 +1,33 @@
+import { useEffect, useMemo } from "react";
 import "./Content.styles.css";
 import { Text } from "../../utils/content";
 import useSearchTextContext from "../../context/SearchTextContext/useSearchTextContext";
 import { highlightText } from "./highlightText";
 
 const Content = () => {
-  const { searchText } = useSearchTextContext();
+  const { searchText, setCount } = useSearchTextContext();
+
+  const { highlightedParagraphs, totalMatches } = useMemo(() => {
+    let matches = 0;
+
+    const paragraphs = Text.map((paragraph) => {
+      const { highlightedText, count } = highlightText(paragraph, searchText);
+      matches += count;
+
+      return highlightedText;
+    });
+
+    return { highlightedParagraphs: paragraphs, totalMatches: matches };
+  }, [searchText]);
+
+  useEffect(() => {
+    setCount(totalMatches);
+  }, [setCount, totalMatches]);
 
   return (
     <div className="content__container">
-      {Text.map((paragraph, index) => (
-        <p key={index}>{highlightText(paragraph, searchText)}</p>
+      {highlightedParagraphs.map((paragraph, index) => (
+        <p key={index}>{paragraph}</p>
       ))}
     </div>
   );
--- src/context/SearchTextContext/index.ts
+++ src/context/SearchTextContext/index.ts
@@ -2,7 +2,9 @@ import { createContext } from "react";
 
 export type SearchTextContextData = {
   searchText: string;
+  count: number;
   setSearchText: React.Dispatch<React.SetStateAction<string>>;
+  setCount: React.Dispatch<React.SetStateAction<number>>;
 };
 
 const SearchTextContext = createContext<SearchTextContextData | undefined>(
